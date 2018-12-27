31a7465b749eae6ae4e74d9267020326f485c706
Joker0727 <1610779207@qq.com>
2018-12-27 09:35:26 +0800

    进程间通信

---
+++ Receiver/frmReceiver.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Forms;
+using CopyDataStruct;
+
+namespace Receiver
+{
+    public partial class frmReceiver : Form
+    {
+        const int WM_COPYDATA = 0x004A;
+        public frmReceiver()
+        {
+            InitializeComponent();
+        }
+
+        protected override void DefWndProc(ref Message m)
+        {
+            switch (m.Msg)
+            {
+                case WM_COPYDATA:
+                    COPYDATASTRUCT cds = new COPYDATASTRUCT();
+                    Type t = cds.GetType();
+                    cds = (COPYDATASTRUCT)m.GetLParam(t);
+                    string strResult = cds.dwData.ToString() + ":" + cds.lpData;
+                    lsvMsgList.Items.Add(strResult);
+                    break;
+                default:
+                    base.DefWndProc(ref m);
+                    break;
+            }
+        }
+    }
+}
+
---
+++ Sender/frmSender.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Sender
+{
+    partial class frmSender
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInt = new System.Windows.Forms.TextBox();
+            this.txtString = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtInt
+            // 
+            this.txtInt.Location = new System.Drawing.Point(48, 53);
+            this.txtInt.Name = "txtInt";
+            this.txtInt.Size = new System.Drawing.Size(100, 21);
+            this.txtInt.TabIndex = 0;
+            // 
+            // txtString
+            // 
+            this.txtString.Location = new System.Drawing.Point(215, 55);
+            this.txtString.Name = "txtString";
+            this.txtString.Size = new System.Drawing.Size(100, 21);
+            this.txtString.TabIndex = 1;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(363, 53);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 2;
+            this.btnSend.Text = "发送";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "int";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(154, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "string";
+            // 
+            // frmSender
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(539, 273);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtString);
+            this.Controls.Add(this.txtInt);
+            this.Name = "frmSender";
+            this.Text = "消息发送者";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInt;
+        private System.Windows.Forms.TextBox txtString;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
